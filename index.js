ba2eb2e5075ed40fa08220548d01f503effd73b6
Enakelemichael <enakelemichael@yahoo.com>
2018-05-14 18:54:41 +0200

    done

---
+++ index.js
@@ -0,0 +1,52 @@
+let groceries = [
+    'Apples', 
+    'Bananas',
+    'Bread',
+    'Milk', 
+    'Butter',
+    'Cheese'
+  ];
+  
+  let amounts = [
+    '2', 
+    '0.5kg',
+    '1',
+    '2l', 
+    '250g',
+    '100g'
+  ];
+  
+  refresh();
+  
+  function refresh() {
+    let $list = $('#list');
+    $list.html('');
+    groceries.forEach(function(item, index) {
+      let $btn = $('<button>')
+        .addClass('remove')
+        .text('X')
+        .click(function() {
+          groceries.splice(index, 1);
+          amounts.splice(index, 1);
+          refresh();
+        });
+      let $line = $('<div>')
+        .addClass('item')
+        .text(item + ', ' + amounts[index])
+        .append($btn); 
+      $list.append($line);
+    });
+  }
+  
+  function addItem() {
+    let $item_input = $('#item-input');
+    let $amount_input = $('#amount-input');
+    
+    groceries.push($item_input.val());
+    amounts.push($amount_input.val());
+  
+    $item_input.val('');
+    $amount_input.val('');
+    
+    refresh();
+  }
